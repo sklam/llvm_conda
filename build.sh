8e14e6ece2778dfcdef553a88ecd5d89431cfacd
Siu Kwan Lam <michael.lam.sk@gmail.com>
2014-11-05 12:31:12 -0600

    Buildscripts now works on osx

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ if [[ (`uname` == Linux) && (`uname -m` != armv6l) ]]
 then
     export CC=gcc44
     export CXX=g++44
-
 fi
 
 if [ `uname` == Darwin ]; then
@@ -15,16 +14,19 @@ export MACOSX_DEPLOYMENT_TARGET=10.7
 
 ./configure \
     --with-python=$SYS_PYTHON \
-    --enable-pic=yes \
-    --enable-optimized=yes \
-    --enable-shared $TARGET \
+    --enable-optimized \
     --prefix=$PREFIX
-make
+
+make -j4
 make install
 
+
 if [ `uname` == Darwin ]; then
-    # FIXME3.5: This script needs to handle renaming symlinks to other renamed files
-    $SYS_PYTHON $RECIPE_DIR/rename_bin_osx.py
+    # Make sure the symbolic paths are relative
+    cd $PREFIX/bin
+    rm llvm-ranlib
+    ln -s llvm-ar llvm-ranlib
+    cd - 
 fi
 
 rm -rf $PREFIX/docs
