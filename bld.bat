ddd60f614564ebd7761047fd5aeb64a40e873b6d
Siu Kwan Lam <michael.lam.sk@gmail.com>
2014-11-06 16:33:09 -0600

    Fix for win64

--- bld.bat
+++ bld.bat
@@ -1,17 +1,17 @@
-if "%ARCH%"=="32" (set MASM_CMD=ml) else (set MASM_CMD=ml64)
+REM if "%ARCH%"=="32" (set MASM_CMD=ml) else (set MASM_CMD=ml64)
 
 mkdir build
 cd build
 
 REM Configure step
-set CMAKE_CUSTOM=-DCMAKE_ASM_MASM_COMPILER=%MASM_CMD% -DLLVM_INCLUDE_TESTS=OFF
-cmake -G "%CMAKE_GENERATOR%" -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% %CMAKE_CUSTOM% %SRC_DIR%
+REM set CMAKE_CUSTOM=-DCMAKE_ASM_MASM_COMPILER=%MASM_CMD% -DLLVM_INCLUDE_TESTS=OFF
+cmake -G "Visual Studio 12 2013 Win64" -DCMAKE_BUILD_TYPE="Release" -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% %CMAKE_CUSTOM% %SRC_DIR%
 if errorlevel 1 exit 1
 
 REM Build step
-devenv %PKG_NAME%.sln /Build "%RELEASE_TARGET%"
+devenv %PKG_NAME%.sln /Build "Release"
 if errorlevel 1 exit 1
 
 REM Install step
-devenv %PKG_NAME%.sln /Build "%RELEASE_TARGET%" /Project INSTALL
+devenv %PKG_NAME%.sln /Build "Release" /Project INSTALL
 if errorlevel 1 exit 1
